0c8a320846fc7bc3b2e03b39d9d003832d6ea571
richardnguyen2504 <gadochannel2504@gmail.com>
2020-11-19 08:34:12 +0700

    CI with second GREEN

--- src/tricuong/utility/MathUtility.java
+++ src/tricuong/utility/MathUtility.java
@@ -5,26 +5,35 @@ package tricuong.utility;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Admin
  */
 public class MathUtility {
-    public static final double PI=3.1415;
-    public static long getFactorial(int n){
-        if (n < 0 || n > 20)
+
+    public static final double PI = 3.1415;
+
+    public static long getFactorial(int n) {
+        if (n < 0 || n > 20) {
             throw new IllegalArgumentException("n must be >= 0 & <= 20");
-        //bắt lỗi input
-        if (n == 0 || n == 1)
-            return 1; //0! 1! = 1
-        
-        long result = 1;
-        
-        for (int i = 2; i <= n; i++) {
-            result *= i;
         }
-        return result;
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * getFactorial(n - 1);
+//         if (n < 0 || n > 20) {
+//            throw new IllegalArgumentException("n must be >= 0 & <= 20");
+//        }
+        //        //bắt lỗi input
+        //        if (n == 0 || n == 1)
+        //            return 1; //0! 1! = 1
+        //        
+        //        long result = 1;
+        //        
+        //        for (int i = 2; i <= n; i++) {
+        //            result *= i;
+        //        }
+        //        return result;
     }
 }
 //Math.sqrt(), .pow
